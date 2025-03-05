76edb5182cc0e8ae9b636e232e1f80691866071b
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-03-04 22:55:32 -0600

    Ignore submodules (#11)
    * Make a list package
    
    * More moving
    
    * Ignore submodules

---
+++ pkg/list/list.go
@@ -0,0 +1,94 @@
+package list
+
+import (
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"slices"
+	"strings"
+)
+
+var Blacklist = []string{
+	"node_modules",
+	"bin", "obj",
+	"pcl",
+	".tdl-old",
+	".uml2ts-old",
+	"testdata",
+	".idea",
+	".vscode",
+	".git",
+}
+
+type Options struct {
+	Absolute     bool
+	ExcludeTests bool
+	Go           bool
+	Proto        bool
+	Typescript   bool
+	CSharp       bool
+	FSharp       bool
+	Dotnet       bool
+}
+
+func (o *Options) sources() []string {
+	sources := []string{}
+	if o.Go {
+		sources = append(sources, ".go")
+	}
+	if o.Proto {
+		sources = append(sources, ".proto")
+	}
+	if o.Typescript {
+		sources = append(sources, ".ts")
+	}
+	if o.Dotnet || o.CSharp {
+		sources = append(sources, ".cs")
+	}
+	if o.Dotnet || o.FSharp {
+		sources = append(sources, ".fs")
+	}
+
+	return sources
+}
+
+func (o *Options) printer(root string) *printer {
+	return &printer{
+		Opts:    o,
+		Sources: o.sources(),
+		Root:    root,
+	}
+}
+
+func Directory(root string, options *Options) error {
+	printer := options.printer(root)
+	return filepath.WalkDir(root,
+		func(path string, d fs.DirEntry, err error) error {
+			if d.IsDir() {
+				if path != root && isSubmodule(path) {
+					fmt.Println("path:", path, "isSubmodule:", isSubmodule(path))
+					return filepath.SkipDir
+				}
+				if blacklisted(path) {
+					return filepath.SkipDir
+				}
+
+				return nil
+			}
+
+			return printer.handle(path)
+		},
+	)
+}
+
+func blacklisted(path string) bool {
+	return slices.ContainsFunc(Blacklist, func(b string) bool {
+		return strings.Contains(path, b)
+	})
+}
+
+func isSubmodule(path string) bool {
+	d, err := os.Stat(filepath.Join(path, ".git"))
+	return err == nil && d.IsDir()
+}
