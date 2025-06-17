c813792697ab62a085342dd909f1f254af681909
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-06-17 10:52:07 -0500

    Export the symbol

--- pkg/make/mk_funcs/main.go
+++ pkg/make/mk_funcs/main.go
@@ -7,6 +7,7 @@ import "C"
 
 import "github.com/unmango/gnumake-go"
 
+//export InitMkFunc
 func InitMkFunc() {
 	gnumake.AddFunction("", func(nm string, argc uint32, argv [][]byte) *byte {
 		return nil
