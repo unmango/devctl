166174a3b6dc011ef216fece21b9a662254cc211
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-06-17 10:46:28 -0500

    Try linking the so

--- test/e2e/mk_funcs_test.go
+++ test/e2e/mk_funcs_test.go
@@ -18,11 +18,14 @@ var _ = Describe("MkFuncs", func() {
 
 		dir = GinkgoT().TempDir()
 		Expect(os.CopyFS(dir, fs)).To(Succeed())
+
+		err = os.Link(mkfuncsSo, filepath.Join(dir, "mk_funcs.so"))
+		Expect(err).NotTo(HaveOccurred())
 	})
 
 	It("should contain the file", func() {
-		_, err := os.Open(filepath.Join(dir, "Makefile"))
+		dirs, err := os.ReadDir(dir)
 		Expect(err).NotTo(HaveOccurred())
-		Expect(mkfuncsSo).To(BeEmpty())
+		Expect(dirs).To(BeEmpty())
 	})
 })
