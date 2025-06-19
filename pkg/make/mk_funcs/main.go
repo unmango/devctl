da4baa51b1edd885e0cbb2f68fbe96c7f1d4b91e
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-06-18 22:51:13 -0500

    Figuring out C stuff

--- pkg/make/mk_funcs/main.go
+++ pkg/make/mk_funcs/main.go
@@ -1,16 +1,25 @@
 package main
 
+/*
+#include <gnumake.h>
+*/
+import "C"
+
 import (
-	"C"
+	"unsafe"
 
 	"github.com/unmango/devctl/pkg/make"
+	"github.com/unmango/gnumake-go"
 )
 
 // https://www.gnu.org/software/make/manual/html_node/load-Directive.html
 
 //export mk_funcs_gmk_setup
-func mk_funcs_gmk_setup() int {
-	make.RegisterFuncs()
+func mk_funcs_gmk_setup(floc *C.gmk_floc) int {
+	gofloc := gnumake.NewFileLocationRef(unsafe.Pointer(floc))
+	gofloc.Deref()
+
+	make.RegisterFuncs(gofloc)
 	return make.SetupSuccess
 }
 
