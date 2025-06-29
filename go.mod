090fbf8379fee53b7e750f02d6e1290d8ffd3ef8
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-06-29 14:08:46 -0500

    gomodtidy

--- go.mod
+++ go.mod
@@ -46,7 +46,6 @@ require (
 	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/spf13/cast v1.7.1 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
-	github.com/spf13/viper v1.20.1 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	go.uber.org/atomic v1.9.0 // indirect
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -5,7 +5,6 @@ import (
 	"github.com/spf13/viper"
 	"github.com/unmango/devctl/pkg/tool"
 	"github.com/unmango/devctl/pkg/work"
-	"gopkg.in/yaml.v3"
 )
 
 const (
@@ -15,13 +14,8 @@ const (
 )
 
 type NotFoundError = viper.ConfigFileNotFoundError
-
 type Config struct {
 	Tools  map[string]tool.Config `json:"tools,omitempty"`
-}
-
-func (c *Config) Unmarshal(data []byte) error {
-	return yaml.Unmarshal(data, c)
 }
 
 var Empty = &Config{}
